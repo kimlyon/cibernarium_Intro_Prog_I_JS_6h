bfe8e1f8f3e8fbf320d56a89ea5cfa7ff692bfd5
Armand Laplana <armando.lap@gmail.com>
2021-01-04 13:48:37 +0100

    4/1/21 final

---
+++ 3__variables.js
@@ -0,0 +1,20 @@
+// VARIABLES 
+// def: Son celdas de memoria donde podemos almacenar diferentes tipos de datos
+
+// TIPOS DE VARIABLES 
+// 1-Numéricas:
+var numero1 = 10;  // declarar e inicializar 
+var numero2 = 5;
+var numero3 = 3.5;
+var numero4 = -5;
+// imprimo el contenido de la variable "numero1"
+console.log(numero1);
+// actualizar contenido de una variable
+numero1 = 12;
+console.log(numero1);
+numero1 = numero1 / 2;
+console.log(numero1);
+
+
+
+
